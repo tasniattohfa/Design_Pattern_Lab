c7e443d2c6bfb1efaf5e48ac28e82d06ba6588be
Tasnia Tasneem Tohfaaa <tasniatasneem@iut-dhaka.edu>
2024-01-04 23:35:38 +0600

    Delete TabletFactory.java

--- TabletFactory.java
+++
@@ -1,5 +0,0 @@
-public class TabletFactory extends GadgetFactory {
-    public Gadget createTablet(String name, String description, double price) {
-        return new Tablet(name, description, price);
-    }
-}
