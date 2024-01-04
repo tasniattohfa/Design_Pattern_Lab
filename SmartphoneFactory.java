eaca8624014ff1abbeb0191a5f29a4cd269b2315
Tasnia Tasneem Tohfaaa <tasniatasneem@iut-dhaka.edu>
2024-01-04 23:40:29 +0600

    Delete SmartphoneFactory.java

--- SmartphoneFactory.java
+++
@@ -1,5 +0,0 @@
-public class SmartphoneFactory extends GadgetFactory {
-    public Gadget createSmartphone(String name, String description, double price) {
-        return new Smartphone(name, description, price);
-    }
-}
